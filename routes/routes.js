8ab108a65dd5e40621abda4488e3017e80f5c14f
Luca Poe <80789505+iamthepoe@users.noreply.github.com>
2022-02-28 20:42:58 -0300

    Add files via upload

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+const User = require('../database/User.js');
 app.get('/', (req,res)=>{
 	res.render('index.ejs');
 });
@@ -9,13 +9,43 @@ app.get('/signin', (req,res)=>{
 	res.render('signin.ejs');
 });
 
+app.get('/login', (req,res)=>{
+	res.render('login.ejs');
+});
+
+app.get('/userlist', (req,res)=>{
+	User.findAll({raw:true, order:[['id', 'DESC']]}).then(user=>{
+		res.render('userslist.ejs',{
+			user: user
+		});
+	});
+});
+
 app.post('/saveaccount', (req,res)=>{
 	let username = req.body.username;
 	let password = req.body.password;
 	let confirmPassword = req.body.confirmPassword;
 	if(password!=confirmPassword){
 		res.render("passworderror.ejs");
+	}else{
+		User.create({
+			username: username,
+			password: password
+		}).then(()=>{
+			res.redirect('/');
+		});
 	}
+});
+
+app.post('/startinglogin', (req,res)=>{
+	let username = req.body.username;
+	let password = req.body.password;
+	console.log(`
+		${username}
+		- - - - - - 
+		${password}
+	`);
+	res.send("This!");
 });
 
 module.exports = app;
